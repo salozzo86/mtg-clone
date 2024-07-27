b142f022042ff7ad9da899a9895c9159edda73d8
Antonio Malafronte <antoniomalafronte@indeed.com>
2024-07-27 08:12:48 +0200

    Refactor GraveyardView

--- src/main/java/views/GraveyardView.java
+++ src/main/java/views/GraveyardView.java
@@ -3,16 +3,19 @@ package views;
 import com.googlecode.lanterna.gui2.GridLayout;
 import com.googlecode.lanterna.gui2.Label;
 import com.googlecode.lanterna.gui2.Panel;
-import lombok.Getter;
 import models.GraveyardModel;
 
-@Getter
 public class GraveyardView {
-    private final Panel panel;
+    private final GraveyardModel graveyardModel;
 
     public GraveyardView(GraveyardModel graveyardModel) {
+        this.graveyardModel = graveyardModel;
+    }
+
+    public Panel getPanel() {
         Label graveyardLabel = new Label("Graveyard: " + graveyardModel.getCards().size());
-        panel = new Panel(new GridLayout(1));
+        final Panel panel = new Panel(new GridLayout(1));
         panel.addComponent(graveyardLabel);
+        return panel;
     }
 }
